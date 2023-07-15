9555098a55d9c2c14aa3ddb7833840d286e7f876
ltx <2050365570@qq.com>
2023-07-15 18:30:14 +0800

    Add Scheduled Task Template Code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # common-template
 1. ## Aop Template Code
+
+2. ## Scheduled Task Template Code
--- src/main/java/com/ltx/Application.java
+++ src/main/java/com/ltx/Application.java
@@ -3,9 +3,16 @@ package com.ltx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+/**
+ * proxyTargetClass = true -> 使用cglib代理(继承)
+ * proxyTargetClass = false -> 使用jdk代理(实现接口)
+ * exposeProxy = true -> 公开代理 -> 使用AopContext.currentProxy()获取代理对象
+ */
+@EnableScheduling //启用Spring的任务调度
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true) //启用AspectJ自动代理
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
