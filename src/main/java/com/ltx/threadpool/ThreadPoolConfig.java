82387b9748a6a522c66d8b63ad4ee1df195b65c7
ltx <2050365570@qq.com>
2023-07-15 21:23:12 +0800

    add ThreadPool template code

---
+++ src/main/java/com/ltx/threadpool/ThreadPoolConfig.java
@@ -0,0 +1,28 @@
+package com.ltx.threadpool;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+
+
+/**
+ * 线程池配置
+ */
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    public ThreadPoolExecutor threadPoolExecutor(ThreadPoolConfigProperties threadPoolConfigProperties) {
+        return new ThreadPoolExecutor(
+                threadPoolConfigProperties.corePoolSize,
+                threadPoolConfigProperties.maximumPoolSize,
+                threadPoolConfigProperties.keepAliveTime,
+                threadPoolConfigProperties.unit,
+                new LinkedBlockingQueue<>(100000), //存储待执行任务的阻塞队列
+                Executors.defaultThreadFactory(), //创建新线程的工厂
+                new ThreadPoolExecutor.AbortPolicy()); //拒绝策略
+    }
+}
---
+++ src/main/java/com/ltx/threadpool/ThreadPoolConfigProperties.java
@@ -0,0 +1,21 @@
+package com.ltx.threadpool;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程池属性
+ */
+@Component
+@ConfigurationProperties(prefix = "my.thread")
+@Data
+public class ThreadPoolConfigProperties {
+    int corePoolSize; //核心线程数
+    int maximumPoolSize; //最大线程数
+    long keepAliveTime; //非核心线程的存活时间
+    TimeUnit unit; //存活时间的时间单位
+}
