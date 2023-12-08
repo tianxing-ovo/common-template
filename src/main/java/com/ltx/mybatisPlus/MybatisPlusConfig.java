4ed0c1ef5cea1fcab20714f1fd23fc9ed4eabbf7
ltx <2050365570@qq.com>
2023-12-09 00:42:11 +0800

    分页插件代码修改

--- src/main/java/com/ltx/mybatisPlus/MybatisPlusConfig.java
+++ src/main/java/com/ltx/mybatisPlus/MybatisPlusConfig.java
@@ -11,16 +11,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * 分页插件
  */
 @Configuration
-@EnableTransactionManagement //开启事务
+@EnableTransactionManagement // 开启事务
 public class MybatisPlusConfig {
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor(){
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
         PaginationInnerInterceptor interceptor = new PaginationInnerInterceptor();
-        //请求页大于最后页，true回到首页，false继续请求，默认为false
+        // 请求页大于最后页,true回到首页,false继续请求,默认为false
         interceptor.setOverflow(true);
-        //设置单页最大数量
+        // 设置单页最大数量
         interceptor.setMaxLimit(10000L);
         mybatisPlusInterceptor.addInnerInterceptor(interceptor);
         return mybatisPlusInterceptor;
