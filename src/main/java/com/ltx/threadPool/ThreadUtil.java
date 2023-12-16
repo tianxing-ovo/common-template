cc9ce2b61bb7135d692413fed3e05b53d0f78a0b
ltx <2050365570@qq.com>
2023-12-16 22:43:01 +0800

    ThreadUtil代码优化

--- src/main/java/com/ltx/threadPool/ThreadUtil.java
+++ src/main/java/com/ltx/threadPool/ThreadUtil.java
@@ -1,9 +1,10 @@
 package com.ltx.threadPool;
 
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ThreadPoolExecutor;
 
 @Component
@@ -15,19 +16,15 @@ public class ThreadUtil {
     /**
      * 无返回值
      */
-    public void execute() {
-        executor.execute(() -> System.out.println(1));
+    public void execute(Runnable runnable) {
+        executor.execute(runnable);
     }
 
     /**
      * 有返回值
      */
-    public Object submit() {
-        try {
-            return executor.submit(() -> 1).get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-        return null;
+    @SneakyThrows
+    public <T> T submit(Callable<T> callable) {
+        return executor.submit(callable).get();
     }
 }
