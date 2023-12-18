711d0ead98f2992c0252b2dbd15a19ee4739da78
ltx <2050365570@qq.com>
2023-12-18 18:47:39 +0800

    跨域配置代码优化

--- src/main/java/com/ltx/interceptor/CorsConfig.java
+++ src/main/java/com/ltx/interceptor/CorsConfig.java
@@ -17,18 +17,17 @@ public class CorsConfig {
      * 获取跨域过滤器
      */
     @Bean
-    public CorsWebFilter corsWebFilter(){
-        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
-        CorsConfiguration configuration=new CorsConfiguration();
-
-        //配置跨域
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        // 配置跨域
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.addAllowedOrigin("*");
-        //允许携带cookie跨域
+        // 允许携带cookie跨域
         configuration.setAllowCredentials(true);
-        //  "/**"表示处理所有的跨域请求
-        source.registerCorsConfiguration("/**",configuration);
+        // "/**"表示处理所有的跨域请求
+        source.registerCorsConfiguration("/**", configuration);
         return new CorsWebFilter(source);
     }
 }
