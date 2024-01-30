47b75ef3ddb834c0791c6ca3a39c3df65b9b03ac
ltx <2050365570@qq.com>
2024-01-30 19:16:57 +0800

    jackson配置

--- src/main/java/com/ltx/jackson/JacksonConfig.java
+++ src/main/java/com/ltx/jackson/JacksonConfig.java
@@ -1,6 +1,7 @@
 package com.ltx.jackson;
 
 import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,9 @@ public class JacksonConfig {
     @Bean
     public JsonMapper jsonMapper() {
         // 忽略大小写
-        return JsonMapper.builder().configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true).build();
+        JsonMapper mapper = JsonMapper.builder().configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true).build();
+        // 下划线命名
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        return mapper;
     }
 }
